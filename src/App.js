d6023a0dc595284b7e690ad216f35280a7ccfd79
Samed GÜRCAN <110107657+samedgrcn@users.noreply.github.com>
2023-10-05 02:40:01 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import { Text, TouchableOpacity, Button } from "react-native";
+import { HStack, VStack, Flex } from "react-native-flex-layout";
+import checkWinner from "./checkWinner";
+
+function Box({ value, onPress, disabled, highlighted }){
+  return(
+    <TouchableOpacity disabled={disabled} onPress={onPress}>
+    <Flex w={100} h={100} center style={{backgroundColor:highlighted ? "lightgreen" : "lightgray"}}>
+      <Text style={{fontSize: 66}}>{value}</Text>
+    </Flex>
+    </TouchableOpacity>
+  )
+}
+
+// <VStack /> -> <View style={{flex: 1, flexDirectiojn: "column"}}/>
+
+function App(){
+
+  const [currentPlayer, setCurrentPlayer] = useState("X");
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [winner,setWinner]=useState(null); 
+  const [highlighted, setHighlighted] = useState([]);
+  
+  const handlePress = (index) => {
+    const newBoard = [...board];
+    newBoard[index] = currentPlayer;
+    setBoard(newBoard);
+    
+    const winnerLine = checkWinner(newBoard)
+    if(winnerLine){
+      setHighlighted(winnerLine);
+      setWinner(currentPlayer);
+      alert(`${currentPlayer} Won!`)
+    }else {
+      setCurrentPlayer(prev => prev === "X" ? "O" : "X")
+    }
+    console.warn(checkWinner(newBoard));
+  }
+
+  const getBox = (index) => (
+    <Box 
+        value={board[index]} 
+        onPress={()=> handlePress(index)}
+        highlighted={highlighted.includes(index)}
+        disabled={winner || board[index]}/>
+  )
+
+  const handleReset = () => {
+    setCurrentPlayer("X");
+    setBoard(Array(9).fill(null));
+    setHighlighted([]);
+    setWinner(null);
+  }
+
+  return (
+    <VStack center spacing={5} style={{flex: 1, flexDirectiojn: "column", marginTop: 180}}>
+      <Text style={{fontSize: 36}}>{currentPlayer} to Play</Text>
+      <HStack spacing={5} shouldWrapChildren>
+        {getBox(0)}
+        {getBox(1)}
+        {getBox(2)}
+      </HStack>
+      <HStack spacing={5} shouldWrapChildren>
+        {getBox(3)}
+        {getBox(4)}
+        {getBox(5)}
+      </HStack>
+      <HStack spacing={5} shouldWrapChildren>
+        {getBox(6)}
+        {getBox(7)}
+        {getBox(8)}
+      </HStack>
+      <Button title="Reset" onPress={handleReset}></Button>
+    </VStack>
+  )
+}
+
+export default App;
